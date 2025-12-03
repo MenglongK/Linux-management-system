733665f916ca2805e709cbe585621c3c5c907c25
MenglongK <menglongkeo07@gmail.com>
2025-12-03 11:12:41 +0700

    Update: script delete & update to avoid delete root user

--- scripts/users/update_user.sh
+++ scripts/users/update_user.sh
@@ -4,6 +4,15 @@ OLD_USERNAME=$1
 NEW_USERNAME=$2
 
 if id "$OLD_USERNAME" &>/dev/null; then
+# Get UID
+USER_UID=$(id -u "$OLD_USERNAME")
+
+# Block root and system accounts and first normal user
+if [[ "$USER_UID" -le 1000 ]]; then
+  echo "Error: Cannot delete root or system accounts and first normal account."
+  exit 1
+fi
+
     sudo usermod -l "$NEW_USERNAME" "$OLD_USERNAME"
     echo "User '$OLD_USERNAME' renamed to '$NEW_USERNAME'"
 else
